a9a318e9a78c3a9dc9c43fe617e672d59a9ba829
admcurtis <mail@adamjcurtis.com>
2026-03-10 15:45:33 +0000

    ns6 to mat converseion script now extract even chans and saves samp numbers to mat struct

--- convert_and_concat.py
+++ convert_and_concat.py
@@ -2,6 +2,7 @@
 # type: ignore
 from scipy.io import savemat
 import numpy as np
+import pandas as pd
 import os
 from pathlib import Path
 from collections import defaultdict
@@ -23,14 +24,13 @@ for f in ns6_files:
     visit = next(part for part in path.parts if "Visit" in part)
     groups[patient][visit].append(f)
 
-
 #%% Process
 for patient, visits in groups.items():
 
     for visit, paths in visits.items():
 
-        visit = visit.replace(" ", "")
         patient = patient.replace(" ", "")
+        visit = visit.replace(" ", "")
 
         # Load all runs for this visit
         visit_data = [NsxFile(f) for f in paths]
@@ -46,6 +46,7 @@ for patient, visits in groups.items():
         signals = [np.array(f["data"]).squeeze() for f in full_data]
         chan_ids = [f["elec_ids"] for f in full_data]
         samp_rates = [float(f["samp_per_s"]) for f in full_data]
+        samples = [data.shape[1] for data in signals]
 
         # Sanity checks
         assert all(sig.shape[0] == signals[0].shape[0] for sig in signals), (
@@ -60,17 +61,31 @@ for patient, visits in groups.items():
             f"Sampling rate differs across runs {patient} {visit}"
         )
 
-        chans = chan_ids[0]
+        # Sample rate
         sr = samp_rates[0]
 
+        # Extract only even numbered chans (odd numbered chans are not brain data)
+        signals_df = [
+            pd.DataFrame(data.T, columns=ids) for data, ids in zip(signals, chan_ids)
+        ]
+        even_chans = [col for col in signals_df[0].columns if int(col) % 2 == 1]
+        even_signals = [np.array(signal[even_chans]).T for signal in signals_df]
+
+        # Concatenate signals for clustering
         print(f"concatenating {patient}, {visit}")
-        combined_signals = np.concatenate(signals, axis=1)
+        combined_signals = np.concatenate(even_signals, axis=1)
 
         # Create .mat structure
-        mat_struct = {"data": combined_signals, "sr": sr}
+        mat_struct = {
+            "data": combined_signals, 
+            "sr": sr,
+            "runs": paths,
+            "samps_per_run": samples
+        }
 
+        # Save
         print(f"Saving .mat data for {patient} {visit}")
-        for i, chan in enumerate(chans):
+        for i, chan in enumerate(even_chans):
 
             save_path = f"processed_data/{patient}/"
             os.makedirs(save_path, exist_ok=True)
