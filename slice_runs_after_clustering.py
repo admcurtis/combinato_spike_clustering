662a37a7773d0c2818d99c6a6d6394dee02ae4cc
admcurtis <mail@adamjcurtis.com>
2026-06-03 19:35:04 +0100

    added script to slice spikes after clustering

--- slice_runs_after_clustering.py
+++ slice_runs_after_clustering.py
@@ -1,12 +1,41 @@
 # %% Dependencies
-import h5py
 import numpy as np
 from scipy.io import loadmat
 from spike_utils import load_spike_data
 
 # %% Load data 
-data = load_spike_data(
-    "/mnt/d/adam_curtis/spike_clustering/processed_data/Patient5/Patient5_Visit1_sensor1",
+spikes, times = load_spike_data(
+    "/mnt/d/adam_curtis/spike_clustering/processed_data/Patient5/Patient5_Visit2_sensor1",
     ppt_num=5,
-    sensor=1
+    sensor=1,
+    visit=2
 )
+
+# %%
+unclustered = loadmat("./processed_data/Patient5/Patient5_Visit2_sensor1.mat")
+sr = int(unclustered["sr"].squeeze())
+runs = unclustered["runs"]
+
+samps_per_run = unclustered["samps_per_run"].squeeze()
+
+slice_times = np.cumsum(samps_per_run)  / sr
+
+new_slices = []
+for i in range(len(slice_times)):
+    
+    start = 0 if i == 0 else slice_times[i - 1]
+    end = slice_times[i]
+
+    time_slice = times[(times >= start) & (times < end)]
+    new_slices.append(time_slice)
+
+
+mat_strcut = {
+    "times": new_slices,
+    "runs": runs
+    
+}
+
+
+
+
